85f82e61d3a95520a9b37403b0bc9deef06a514f
SivaTeja <66307185+SivaTeja60@users.noreply.github.com>
2022-01-13 20:05:25 -0800

    Update duplicateCheck.py

--- LeetCode/duplicateCheck/duplicateCheck.py
+++ LeetCode/duplicateCheck/duplicateCheck.py
@@ -1,5 +1,16 @@
+# Using Set
+# Set doesnt allow duplicates
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         if len(nums) == len(set(nums)):
+            return False
+        return True
+    
+# Using Dictionary
+# Dictonary doesnt allow duplicates
+    class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        res=dict.fromkeys(nums)
+        if len(res)==len(nums):
             return False
         return True
