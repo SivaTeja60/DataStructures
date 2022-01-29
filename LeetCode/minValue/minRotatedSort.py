7e4ac48f5cb5d31e718a81e1435d376897134e92
SivaTeja <66307185+SivaTeja60@users.noreply.github.com>
2022-01-29 13:28:22 -0800

    Update minRotatedSort.py

--- LeetCode/minValue/minRotatedSort.py
+++ LeetCode/minValue/minRotatedSort.py
@@ -1,3 +1,15 @@
+#N- Time Complexity
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        min=nums[0]
+        l=len(nums)-1
+        if (nums[0]<nums[l]) or l==0:
+            return nums[0]
+        for i in range(0,l):
+            if nums[i+1]<nums[i]:
+                return nums[i+1]
+            
+# Logn Time Complexity
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         l = 0
